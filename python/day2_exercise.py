1fd1e675c9144b68498656e90826caaf483b4530
tahimaaz31 <tahirmaaz799@gmail.com>
2025-09-03 03:09:55 +0200

    variable

---
+++ python/day2_exercise.py
@@ -0,0 +1,27 @@
+# variable
+
+name = "Tahir"
+age = 25
+city = "Budapest"
+
+print("My name is", name, "i am",age, "years old.I lives in", city,)
+
+#Swapping values
+x = 10
+y = 20
+
+x,y = y,x
+print(x, y)
+
+#students marks
+
+math = 80
+science = 90
+english = 85
+total = english+ science+math
+average = total/3
+
+print("Total Numbers",total)
+print("Average", average)
+
+
